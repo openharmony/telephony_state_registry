8baa1e0a61835f38214dd6ed857559d2f508316b
wulimm <hanjing5@huawei.com>
2024-05-31 08:12:47 +0000

    条件竞争问题整改
    Signed-off-by: wulimm <hanjing5@huawei.com>

--- services/src/telephony_state_registry_service.cpp
+++ services/src/telephony_state_registry_service.cpp
@@ -152,8 +152,8 @@ int32_t TelephonyStateRegistryService::UpdateCellularDataFlow(int32_t slotId, in
         TELEPHONY_LOGE("Check permission failed.");
         return TELEPHONY_STATE_REGISTRY_PERMISSION_DENIED;
     }
-    cellularDataFlow_[slotId] = flowData;
     std::lock_guard<std::mutex> guard(lock_);
+    cellularDataFlow_[slotId] = flowData;
     int32_t result = TELEPHONY_STATE_REGISTRY_DATA_NOT_EXIST;
     for (size_t i = 0; i < stateRecords_.size(); i++) {
         TelephonyStateRegistryRecord record = stateRecords_[i];
@@ -679,6 +679,7 @@ int TelephonyStateRegistryService::Dump(std::int32_t fd, const std::vector<std::
     }
     std::string result;
     TelephonyStateRegistryDumpHelper dumpHelper;
+    std::lock_guard<std::mutex> guard(lock_);
     if (dumpHelper.Dump(argsInStr, stateRecords_, result)) {
         std::int32_t ret = dprintf(fd, "%s", result.c_str());
         if (ret < 0) {
