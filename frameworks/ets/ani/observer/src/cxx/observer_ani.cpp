9bcd2d0e3e3cd656ce928fddab25336deeb70e08
niumingyu0 <niumingyu@huawei.com>
2025-08-12 11:36:30 +0000

    update frameworks/ets/ani/observer/src/cxx/observer_ani.cpp.
    Signed-off-by: niumingyu0 <niumingyu@huawei.com>

--- frameworks/ets/ani/observer/src/cxx/observer_ani.cpp
+++ frameworks/ets/ani/observer/src/cxx/observer_ani.cpp
@@ -73,12 +73,12 @@ void AniTelephonyObserver::OnNetworkStateUpdated(int32_t slotId, const sptr<Tele
     on_network_state_updated(slotId, info);
 }
 
-void AniTelephonyObserver::OnCallStateUpdated(int32_t slotId, int32_t callState, const std::u16string &num)
+void AniTelephonyObserver::OnCallStateUpdated(int32_t slotId, int32_t callState, const std::u16string &teleNumber)
 {
-    std::string phone_number = NapiUtil::ToUtf8(num);
+    std::string phone_number = NapiUtil::ToUtf8(teleNumber);
     CallStateAni info{
         .state = callState,
-        .num = rust::String(phone_number),
+        .teleNumber = rust::String(phone_number),
     };
     on_call_state_updated(slotId, info);
 }
