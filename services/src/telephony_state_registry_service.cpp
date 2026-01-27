4056ab273db6cd960c12551fe79ef8e1d2cc6de0
xjsun <sunxiaojun1@huawei.com>
2026-01-27 16:24:16 +0800

    fix bug for ccallstateChange
    Signed-off-by: xjsun <sunxiaojun1@huawei.com>

--- services/src/telephony_state_registry_service.cpp
+++ services/src/telephony_state_registry_service.cpp
@@ -684,8 +684,12 @@ void TelephonyStateRegistryService::UpdateDataEx(const TelephonyStateRegistryRec
     if ((record.mask_ & TelephonyObserverBroker::OBSERVER_MASK_CCALL_STATE) != 0) {
         if (record.CanManageCallForDevices()) {
             TELEPHONY_LOGI("RegisterStateChange##Notify-OBSERVER_MASK_CCALL_STATE");
-            record.telephonyObserver_->OnCCallStateUpdated(record.slotId_,
-                callState_[record.slotId_], callIncomingNumber_[record.slotId_]);
+            int32_t callState = callState_[record.slotId_];
+            if (callState == static_cast<int32_t>(CallState::CALL_STATE_UNKNOWN)) {
+                callState = static_cast<int32_t>(CallState::CALL_STATE_IDLE);
+            }
+            record.telephonyObserver_->OnCCallStateUpdated(record.slotId_, callState,
+                callIncomingNumber_[record.slotId_]);
         }
     }
 }
