70cb17bd1693d58b4ea7cf3acc3a235448513e1f
shao-yikai <shaoyikai6@huawei.com>
2026-07-26 22:18:17 +0800

    update: 更新文件 log.rs
    Signed-off-by: shao-yikai <shaoyikai6@huawei.com>

--- frameworks/ets/ani/observer/src/log.rs
+++ frameworks/ets/ani/observer/src/log.rs
@@ -11,7 +11,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 #[macro_export]
 macro_rules! telephony_debug {
     ($fmt: literal $(, $args:expr)* $(,)?) => {{
