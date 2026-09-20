f4c3002313645fd51a1ce5a5295ea16a6d3ff3f4
fanrui0 <fanrui32@h-partners.com>
2026-09-20 14:36:38 +0800

    update: 更新文件 napi_telephony_observer.cpp
    Signed-off-by: fanrui0 <fanrui32@h-partners.com>

--- frameworks/js/napi/src/napi_telephony_observer.cpp
+++ frameworks/js/napi/src/napi_telephony_observer.cpp
@@ -17,6 +17,7 @@
 #include "event_listener_manager.h"
 #include "telephony_callback_event_id.h"
 #include "telephony_log_wrapper.h"
+#include "telephony_types.h"
 #include "update_infos.h"
 
 namespace OHOS {
@@ -182,6 +183,19 @@ void NapiTelephonyObserver::OnSimActiveStateUpdated(int32_t slotId, bool enable)
         ToUint32t(TelephonyCallbackEventId::EVENT_ON_SIM_ACTIVE_STATE_UPDATE), simActiveStateUpdateInfo);
 }
 
-void
+void NapiTelephonyObserver::OnVoIPStateUpdated(const VoIPCallStateInfo &info)
+{
+    TELEPHONY_LOGI("OnVoIPStateUpdated callState = %{public}d, callType = %{public}d",
+        static_cast<int32_t>(info.callState), static_cast<int32_t>(info.callType));
+    std::unique_ptr<VoIPCallStateUpdateInfo> voipCallStateUpdateInfo =
+        std::make_unique<VoIPCallStateUpdateInfo>(DEFAULT_SIM_SLOT_ID, info.appName, info.contactName,
+            info.callType, info.callState, info.isVoiceAnswerSupported);
+    if (voipCallStateUpdateInfo == nullptr) {
+        TELEPHONY_LOGE("voipCallStateUpdateInfo is nullptr!");
+        return;
+    }
+    EventListenerManager::SendEvent(
+        ToUint32t(TelephonyCallbackEventId::EVENT_ON_VOIP_CALL_STATE_UPDATE), voipCallStateUpdateInfo);
+}
 } // namespace Telephony
 } // namespace OHOS
